4b9d5702b5693c20c255cfcb879d68f3f0eb1968
Leo Barnett <leo.e.barnett@icloud.com>
2024-01-06 15:23:27 +0200

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Leo  on 06.01.24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
